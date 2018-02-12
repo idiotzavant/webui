7177856f4ccf3a18175f3ce71c501ae63a1f4154
eigen field <idiotzavant@gmail.com>
2018-02-12 13:13:46 +0800

    add templates and menu routes ready

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms import \
     BooleanField,\
     SubmitField,\
     SelectField
-from wtforms.validators import DataRequired,IPAddress,Length
+from wtforms.validators import DataRequired,IPAddress,Length,NumberRange
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -21,4 +21,77 @@ class LANConfiguration(FlaskForm):
     default_gateway_address  = StringField(u'Default Gateway Address', validators=[DataRequired(),IPAddress()])
     primary_dns_ip_address  = StringField(u'Primary DNS IP Address', validators=[DataRequired(),IPAddress()])
     secondary_dns_ip_address  = StringField(u'Secondary DNS IP Address', validators=[DataRequired(),IPAddress()])
+    submit = SubmitField('Update')
+
+class SystemInformation(FlaskForm):
+    system_contact  = StringField('System Contact', validators=[])
+    system_location  = StringField('System Location', validators=[])
+    system_name  = StringField('System Name', validators=[])
+    serial_number  = StringField('Serial Number', validators=[])
+    submit = SubmitField('Update')
+
+class Jamming(FlaskForm):
+    timeout  = StringField('Timeout', validators=[NumberRange()])
+    submit = SubmitField('Update')
+
+class WLANAttack(FlaskForm):
+    timeout  = StringField('Timeout', validators=[NumberRange()])
+    submit = SubmitField('Update')
+
+class BruteForceLogin(FlaskForm):
+    timeout  = StringField('Timeout', validators=[NumberRange()])
+    duration  = StringField('Duration', validators=[NumberRange()])
+    count  = StringField('Count', validators=[NumberRange()])
+    submit = SubmitField('Update')
+
+class RadiusLogin(FlaskForm):
+    timeout  = StringField('Timeout', validators=[NumberRange()])
+    duration  = StringField('Duration', validators=[NumberRange()])
+    count  = StringField('Count', validators=[NumberRange()])
+    submit = SubmitField('Update')
+
+def getdefault():
+    return 2222
+
+class ServicesCli(FlaskForm):
+    mode_options = [('1', 'Enable'), ('2', 'Disable')]
+    clish_mode = SelectField('Mode',choices = mode_options)
+    ssh_mode = SelectField('Mode',choices = mode_options)
+    ssh_port = StringField('Port',validators=[DataRequired(),NumberRange()])
+    submit = SubmitField('Update')
+
+class ServicesDhcp(FlaskForm):
+    mode_options = [('1', 'Enable'), ('2', 'Disable')]
+    mode = SelectField('Mode',choices = mode_options)
+    subnet_mask  = StringField(u'Subnet Mask', validators=[DataRequired(),IPAddress()])
+    gateway_ip  = StringField(u'Gateway IP', validators=[DataRequired(),IPAddress()])
+    primary_dns_ip  = StringField(u'Primary DNS IP', validators=[DataRequired(),IPAddress()])
+    secondary_dns_ip  = StringField(u'Secondary DNS IP', validators=[DataRequired(),IPAddress()])
+    submit = SubmitField('Update')
+
+class ServicesWebServer(FlaskForm):
+    submit = SubmitField('Update')
+
+class ServicesSnmp(FlaskForm):
+    submit = SubmitField('Update')
+
+class SystemDeviceMaintenance(FlaskForm):
+    submit = SubmitField('Update')
+
+class SystemLog(FlaskForm):
+    submit = SubmitField('Update')
+
+class SystemStatus(FlaskForm):
+    submit = SubmitField('Update')
+
+class SystemStatistics(FlaskForm):
+    submit = SubmitField('Update')
+
+class GraylogStatus(FlaskForm):
+    submit = SubmitField('Update')
+
+class AlertsMonitor(FlaskForm):
+    submit = SubmitField('Update')
+
+class ComponentUpgrade(FlaskForm):
     submit = SubmitField('Update')
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,147 @@
 from app import app
 from flask import render_template,flash,redirect,url_for,request
-from app.forms import LoginForm,LANConfiguration
 from flask_login import current_user, login_user, logout_user,login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from app.forms import \
+    LoginForm,\
+    LANConfiguration,\
+    SystemInformation,\
+    Jamming,\
+    WLANAttack,\
+    BruteForceLogin,\
+    RadiusLogin,\
+    ServicesCli,\
+    ServicesDhcp,\
+    ServicesWebServer,\
+    ServicesSnmp,\
+    SystemDeviceMaintenance,\
+    SystemStatus,\
+    SystemStatistics,\
+    GraylogStatus,\
+    AlertsMonitor,\
+    ComponentUpgrade,\
+    SystemLog
 
 ######################################################
-@app.route('/basic/networksetting',methods=['GET','POST'])
-def basic_networksetting():
-    form = LANConfiguration()
-    if form.validate_on_submit():
-        flash("success network settings")
-        print("ip_address = {}".format(form.ip_address.data))
-        return redirect(url_for('siem'))
-    return render_template('lanconfiguration.html',form=form)
+@app.route('/basic/<submenu>',methods=['GET','POST'])
+def basic(submenu):
+    print("* " + submenu)
+    if submenu == 'networksetting':
+        form = LANConfiguration()
+        if form.validate_on_submit():
+            flash("success network settings")
+            print("ip_address = {}".format(form.ip_address.data))
+            return redirect(url_for('siem'))
+        return render_template('lanconfiguration.html',form=form)
+    elif submenu == 'administration':
+        form = SystemInformation()
+        if form.validate_on_submit():
+            flash("success sys administration settings")
+            return redirect(url_for('siem'))
+        return render_template('systeminformation.html',form=form)
 
+@app.route('/algo/<submenu>',methods=['GET','POST'])
+def algo(submenu):
+    if submenu == 'jamming':
+        form = Jamming()
+        if form.validate_on_submit():
+            flash("success jamming settings")
+            return redirect(url_for('siem'))
+        return render_template('jamming.html',form=form)
+    elif submenu == 'wlanattack':
+        form = WLANAttack()
+        if form.validate_on_submit():
+            flash("success WLAN ATtack settings")
+            return redirect(url_for('siem'))
+        return render_template('wlanattack.html',form=form)
+    elif submenu == 'bruteforcelogin':
+        form = BruteForceLogin()
+        if form.validate_on_submit():
+            flash("success Brute Force Login settings")
+            return redirect(url_for('siem'))
+        return render_template('bruteforcelogin.html',form=form)
+    elif submenu == 'radiuslogin':
+        form = RadiusLogin()
+        if form.validate_on_submit():
+            flash("success Radius Login settings")
+            return redirect(url_for('siem'))
+        return render_template('radiuslogin.html',form=form)
+
+@app.route('/services/<submenu>',methods=['GET','POST'])
+def services(submenu):
+    if submenu == 'cli':
+        form = ServicesCli()
+
+        if request.method == 'GET':
+            form.clish_mode.default = 1
+            form.ssh_mode.default = 2
+            form.ssh_port.default = 8888
+            form.process()
+
+        if form.validate_on_submit(): 
+            print("SSH PORT = " + form.ssh_port.data)
+            return redirect(url_for('siem'))    
+        return render_template('servicescli.html',form=form)
+    elif submenu == 'dhcp':
+        form = ServicesDhcp()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('servicesdhcp.html',form=form)
+    elif submenu == 'webserver':
+        form = ServicesWebServer()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('serviceswebserver.html',form=form)
+    elif submenu == 'snmp':
+        form = ServicesSnmp()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('servicessnmp.html',form=form)
+
+@app.route('/system/<submenu>',methods=['GET','POST'])
+def system(submenu):
+    if submenu == 'devicemaintenance':
+        form = SystemDeviceMaintenance()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('systemdevicemaintenance.html',form=form)
+    elif submenu == 'systemlog':
+        form = SystemLog()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('systemlog.html',form=form)
+
+@app.route('/status/<submenu>',methods=['GET','POST'])
+def status(submenu):
+    if submenu == 'systemstatus':
+        form = SystemStatus()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('systemstatus.html',form=form)
+    elif submenu == 'systemstatistics':
+        form = SystemStatistics()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('systemstatistics.html',form=form)
+    elif submenu == 'graylogstatus':
+        form = GraylogStatus()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('graylogstatus.html',form=form)
+    elif submenu == 'alertsmonitor':
+        form = AlertsMonitor()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('alertsmonitor.html',form=form)
+
+@app.route('/superuser/<submenu>',methods=['GET','POST'])
+def superuser(submenu):
+    if submenu == 'componentupgrade':
+        form = ComponentUpgrade()
+        if form.validate_on_submit():
+            return redirect(url_for('siem'))
+        return render_template('componentupgrade.html',form=form)
 ######################################################
 
 @app.route('/logout')
